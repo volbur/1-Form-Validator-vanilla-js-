9041c8f4889141bb080d41337a70a51c7eb649f5
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-23 21:02:02 +0300

    [script.js] addid function for check passwords match

--- script.js
+++ script.js
@@ -62,6 +62,13 @@ function checkLength(input, min, max) {
   }
 }
 
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
 // Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
