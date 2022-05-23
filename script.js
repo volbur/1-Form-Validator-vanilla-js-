13d6a0d78e8b5b6f49c6725d711390605eedaf96
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-23 20:54:02 +0300

    [script.js] addid event listeners

--- script.js
+++ script.js
@@ -66,3 +66,15 @@ function checkLength(input, min, max) {
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if(checkRequired([username, email, password, password2])) {
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2)
+  }
+})
