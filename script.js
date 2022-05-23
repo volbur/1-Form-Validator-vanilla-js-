6dbba62648b827e4894ae2044d685d2dc639d9cd
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-23 20:42:14 +0300

    [script.js] created function for check input length

--- script.js
+++ script.js
@@ -44,3 +44,20 @@ function checkRequired() {
 
   return isRequired;
 }
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
