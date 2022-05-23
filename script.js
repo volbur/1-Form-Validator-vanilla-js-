f9cff68514f51a6f684ecf40baaefb55d2fbb8ed
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-23 19:25:42 +0300

    [script.js] create refs

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
