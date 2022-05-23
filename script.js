3e1d9413f573fb8f7d0abf2a3562bf064bbf1e31
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-23 21:07:22 +0300

    [script.js] addid params for showSuccess and checkRequired

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ function checkEmail(input) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
-    showSuccess();
+    showSuccess(input);
   } else {
     showError(input, "Email is not valid");
   }
 }
 
 // Check required fields 
-function checkRequired() {
+function checkRequired(inputArr) {
   let isRequired = false;
   inputArr.forEach(input => {
     if (input.value.trim() === '') {
