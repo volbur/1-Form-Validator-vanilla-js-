48f7c8b3efd1fc73d47bd3b717b5a1cc282ac564
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-23 19:36:11 +0300

    [script.js] created function for showError

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerTex = message;
+}
